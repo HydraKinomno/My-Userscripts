3ee1cd596f0def46d58f739762fcb20accda354e
HydraKinomno <gameejay2662@outlook.com>
2024-07-14 14:20:55 +0600

    chonky
    chonly

---
+++ Double Sub count remover-2024-07-14.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Double Sub count remover
+// @namespace    http://tampermonkey.net/
+// @version      2024-07-14
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.youtube.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        none
+// ==/UserScript==
+```[exp-channels3] div.c3-header-left.flex-bar > div#c3-subscribe.c3-subscribe > span.yt-uix-button-subscription-container.with-preferences > span.yt-subscription-button-subscriber-count-branded-horizontal.subscribed
+    { display: none !important;}
+    [exp-channels3] div.c3-header-left.flex-bar > div#c3-subscribe.c3-subscribe > span.yt-uix-button-subscription-container.with-preferences > span.yt-subscription-button-subscriber-count-branded-horizontal
+    { display: none !important;}```
